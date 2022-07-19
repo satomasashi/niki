5cbbf00dc0347cf5c150601de6359282260a7947
satomasashi <vmomv@hotmail.com>
2022-07-19 16:58:15 +0900

    spring initilizrでプロジェクト作成後にステージングとコミット

---
+++ niki/src/test/java/per/mark/niki/NikiApplicationTests.java
@@ -0,0 +1,13 @@
+package per.mark.niki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
